3b6f6024af907a760176210afb9c7dd59ffd38bb
Penn York <markyan@foxmail.com>
2025-06-23 16:17:32 +0800

    refactor: simplify error handling in query client; remove unnecessary HTTPException checks and update .gitignore to exclude .tanstack directory

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -1,5 +1,4 @@
 import { MutationCache, QueryCache, QueryClient } from "@tanstack/react-query";
-import { HTTPException } from "hono/http-exception";
 import { toast } from "sonner";
 
 export const createQueryClient = () => {
@@ -23,16 +22,12 @@ export const createQueryClient = () => {
 		},
 		queryCache: new QueryCache({
 			onError: (err) => {
-				if (err instanceof HTTPException) {
-					toast.error(err.message);
-				}
+				toast.error(err.message);
 			},
 		}),
 		mutationCache: new MutationCache({
 			onError: (err) => {
-				if (err instanceof HTTPException) {
-					toast.error(err.message);
-				}
+				toast.error(err.message);
 			},
 		}),
 	});
